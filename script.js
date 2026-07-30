ac1b22d6134883a33bad0023292d6a097b3fa7a5
yamenibrahim2011-debug <yamenibrahim2011@gmail.com>
2026-07-31 01:48:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function startSurprise() {
     document.querySelector(".container").style.display = "none";
 
     const message = document.getElementById("message");
-
     message.classList.remove("hidden");
+
+    const music = document.getElementById("birthdayMusic");
+
+    music.play();
 
 }
