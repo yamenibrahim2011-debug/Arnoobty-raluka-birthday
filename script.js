f9bd0a271f4fed4ee1c5407c02f592540609798c
yamenibrahim2011-debug <yamenibrahim2011@gmail.com>
2026-07-31 02:10:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
+let currentMemory = 1;
+
 function startSurprise() {
 
     const music = document.getElementById("birthdayMusic");
 
     music.volume = 0.5;
 
-    music.play()
-    .then(() => {
-        console.log("Music started");
-    })
-    .catch(error => {
-        console.log("Music error:", error);
-    });
+    music.play();
 
 
     document.querySelector(".container").style.display = "none";
@@ -22,7 +18,34 @@ function startSurprise() {
 
 
     setTimeout(() => {
+
         document.getElementById("gallery").classList.remove("hidden");
+
+        showMemories();
+
     }, 10000);
+
+}
+
+
+function showMemories() {
+
+    const image = document.getElementById("memoryImage");
+    const text = document.getElementById("memoryText");
+
+
+    setInterval(() => {
+
+        currentMemory++;
+
+        if(currentMemory <= 24){
+
+            image.src = "photos/photo" + currentMemory + ".jpg";
+
+            text.innerHTML = "Memory " + currentMemory + " 🤍";
+
+        }
+
+    }, 4000);
 
 }
