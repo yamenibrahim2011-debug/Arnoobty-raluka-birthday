0d2492e4e0c03da7024dc3e4fb1dd894d4341304
yamenibrahim2011-debug <yamenibrahim2011@gmail.com>
2026-07-31 01:52:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function startSurprise() {
 
+    const music = document.getElementById("birthdayMusic");
+
+    music.volume = 0.5;
+    music.play()
+        .then(() => {
+            console.log("Music started");
+        })
+        .catch((error) => {
+            console.log("Music error:", error);
+        });
+
+
     document.querySelector(".container").style.display = "none";
 
     const message = document.getElementById("message");
     message.classList.remove("hidden");
-
-    const music = document.getElementById("birthdayMusic");
-
-    music.play();
 
 }
